5ea2842c1cd7373ef48f53a72ee77e0220a31e13
castilho-amaral <augustoamaral73@gmail.com>
2025-01-06 13:50:18 -0300

    formatação da saida do log para o formato JSON

--- check_nginx_status.sh
+++ check_nginx_status.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-LOG_ONLINE="/nginx_status/online"
-LOG_OFFLINE="/nginx_status/offline"
+LOG_ONLINE="/nginx_status/online/"
+LOG_OFFLINE="/nginx_status/offline/"
 SERVICE_NAME="nginx"
 
 STATUS=$(systemctl is-active $SERVICE_NAME)
 
-DATE_TIME=$(date +'%Y-%m-%d_%H-%M-%S')
-
-MESSAGE="Verifica o status do $SERVICE_NAME"
+DATE_TIME=$(date +'%H-%M_%d-%m-%Y')
+MESSAGE="Verificação do status do serviço $SERVICE_NAME"
 
 if [ "$STATUS" == "active" ]; then
 
-  LOG_ONLINE="$LOG_ONLINE/nginx_status_online_$DATE_TIME.log"
-  echo "$DATE_TIME - $MESSAGE - Status: ONLINE" > "$LOG_ONLINE"
+  json=$(cat <<EOF
+    {
+       "timestamp": "$DATE_TIME",
+       "message": "$MESSAGE",
+       "status":  "$STATUS"
+    }
+EOF
+)
+  LOG_ONLINE="$LOG_ONLINE/nginx_status.json"
+  echo "$json" >> "$LOG_ONLINE"
 else
-  LOG_OFFLINE="$LOG_OFFLINE/nginx_status_offline_$DATE_TIME.log"
-  echo "$DATE_TIME - $MESSAGE - Status: OFFLINE" > "$LOG_OFFLINE"
+
+  json=$(cat <<EOF
+    {
+       "timestamp": "$DATE_TIME",
+       "message": "$MESSAGE",
+       "status":  "$STATUS"
+    }
+EOF
+)
+  LOG_OFFLINE="$LOG_OFFLINE/nginx_status.json"
+  echo "$json" >> "$LOG_OFFLINE"
 fi
